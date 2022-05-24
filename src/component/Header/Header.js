0f61d7c7f72c087dfc09b851bba8683dbdd9bcbd
Ivan Starikov <69046400+Ivan-Starikov@users.noreply.github.com>
2022-05-24 22:40:53 +0400

    Add global styles and Header component (#1)

---
+++ src/component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Header = () => {
+   return (
+    <Title>Title</Title>
+   )
+};
+
+export default Header;
---
+++ src/component/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+  background-color: #ECDBBA;
+  height: 10rem;
+`
+
